a3c90bef026945c99be805966b150ff2a2096532
valenciaruben <60829651+valenciaruben@users.noreply.github.com>
2021-03-07 21:03:02 -0500

    Update and rename correo.php to mail.php

--- mail.php
+++ mail.php
@@ -1,8 +1,8 @@
 <?php
   $destino="valenciaruben@gmail.com";
-  $asunto="Enviado desde rubenvalenciajimenez.000webhost.com";
+  $asunto="Enviado desde https://keen-kilby-f47203.netlify.app";
   $nombre=$_POST['nombre'];
-  $correo=$_POST['correo'];
+  $correo=$_POST['email'];
   $mensaje=$_POST['mensaje'];
   $contenido="Nombre: ".$nombre."\nCorreo: ".$correo."\nMensaje: ".$mensaje;
   if(mail($destino,$asunto,$contenido)){
@@ -10,5 +10,5 @@
   }else{
     echo "No enviado. Intente más tarde... ";
   }
-  echo "<a onclick='javascript:mywindow.close();' href='https://guadalupanolasalle.edu.co'>Regresar</a>";
+  //echo "<a onclick='javascript:mywindow.close();' href='https://guadalupanolasalle.edu.co'>Regresar</a>";
 ?>
